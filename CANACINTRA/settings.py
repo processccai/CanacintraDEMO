a7a0836af0126286c9c6df4dc50cad0537de6b95
ChinoISC <oscar010394@gmail.com>
2023-11-15 20:39:47 -0600

    Add Email Register

--- CANACINTRA/settings.py
+++ CANACINTRA/settings.py
@@ -147,3 +147,10 @@ if not DEBUG:
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'processautomedccai@gmail.com'  # Cambia esto al correo de Gmail desde el que quieres enviar correos
+EMAIL_HOST_PASSWORD = 'uvgk egpg gcyl kafi '  
